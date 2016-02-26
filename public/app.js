8d684ce56b4cfc342b88f92ee3cfc2139f16561f
Walter Scarborough <walter.scarborough@gmail.com>
2016-02-25 21:33:41 -0600

    Added file handling + basic parsing behavior

--- public/app.js
+++ public/app.js
@@ -1,2 +1,48 @@
+
+'use strict';
+
+// NOTE: we should consider submitting this as a patch to papaparse
+// Based on: http://stackoverflow.com/questions/31375531/how-to-use-promises-with-papaparse
+Papa.parsePromise = function(file) {
+    return new Promise(function(resolve, reject) {
+        Papa.parse(file, 
+            { 
+                complete: resolve,
+                error: reject,
+            }
+        );
+    });
+};
+
 $(document).ready(function () {
+    $('#file-upload').on('change', function(e) {
+
+        let file = this.files[0];
+
+        $('#filename').text(file.name);
+
+        Papa.parsePromise(file)
+            .then(function(results) {
+
+                let headerData = results.data.shift();
+
+                // Set column titles
+                headerData.forEach(function(result) {
+                    $('#data-table thead tr').append('<th>' + result + '</th>');
+                });
+
+                // Set row data
+                results.data.forEach(function(rowData) {
+
+                    $('#data-table tbody').append('<tr>');
+
+                    rowData.forEach(function(rowItem) {
+                        $('#data-table tbody').append('<td>' + rowItem + '</td>');
+                    });
+                    
+                    $('#data-table tbody').append('</tr>');
+                });
+            })
+            ;
+    });
 });
